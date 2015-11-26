cc0a82008aa539acf4f95db8f32ab26f6db2ec0f
Jonas Erikson <jonas@redeploy.se>
2015-11-27 00:05:20 +0100

    commit

--- quick-redeploy-unifi-controller/echo.sh
+++ quick-redeploy-unifi-controller/echo.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo Hello World > /tmp/HeloWorld
+echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-get update
+sudo apt-get install unifi -y
