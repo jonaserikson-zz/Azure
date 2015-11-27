aea22d76fdbd3f9d4b3613a8e032c259902a2b7a
Jonas Erikson <jonas@redeploy.se>
2015-11-27 11:20:55 +0100

    commit

--- quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -3,3 +3,10 @@ echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo apt-get update
 sudo apt-get install unifi -y
+echo "echo \"\"" >> /etc/profile
+echo "echo \"\"" >> /etc/profile
+echo "echo \"\"" >> /etc/profile
+echo "echo \"Your management URL for your Unifi Controller is https://\`curl -s ipecho.net/plain; echo\`:8443\"" >> /etc/profile
+echo "echo \"\"" >> /etc/profile
+echo "echo \"\"" >> /etc/profile
+echo "echo \"\"" >> /etc/profile
