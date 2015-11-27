eef276d3fa866a0ecd051001db2c53ac0527aca5
Jonas Erikson <jonas@redeploy.se>
2015-11-27 14:34:26 +0100

    commit

--- quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -5,8 +5,17 @@ sudo apt-get update
 sudo apt-get install unifi -y
 echo "echo \"\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
+echo "echo \"██▀███  ▓█████ ▓█████▄ ▓█████  ██▓███   ██▓     ▒█████  ▓██   ██▓\"" >> /etc/profile
+echo "echo \"▓██ ▒ ██▒▓█   ▀ ▒██▀ ██▌▓█   ▀ ▓██░  ██▒▓██▒    ▒██▒  ██▒ ▒██  ██▒\"" >> /etc/profile
+echo "echo \"▓██ ░▄█ ▒▒███   ░██   █▌▒███   ▓██░ ██▓▒▒██░    ▒██░  ██▒  ▒██ ██░\"" >> /etc/profile
+echo "echo \"▒██▀▀█▄  ▒▓█  ▄ ░▓█▄   ▌▒▓█  ▄ ▒██▄█▓▒ ▒▒██░    ▒██   ██░  ░ ▐██▓░\"" >> /etc/profile
+echo "echo \"░██▓ ▒██▒░▒████▒░▒████▓ ░▒████▒▒██▒ ░  ░░██████▒░ ████▓▒░  ░ ██▒▓░\"" >> /etc/profile
+echo "echo \"░ ▒▓ ░▒▓░░░ ▒░ ░ ▒▒▓  ▒ ░░ ▒░ ░▒▓▒░ ░  ░░ ▒░▓  ░░ ▒░▒░▒░    ██▒▒▒\"" >> /etc/profile
+echo "echo \" ░▒ ░ ▒░ ░ ░  ░ ░ ▒  ▒  ░ ░  ░░▒ ░     ░ ░ ▒  ░  ░ ▒ ▒░  ▓██ ░▒░\"" >> /etc/profile
+echo "echo \" ░░   ░    ░    ░ ░  ░    ░   ░░         ░ ░   ░ ░ ░ ▒   ▒ ▒ ░░\"" >> /etc/profile
+echo "echo \"  ░        ░  ░   ░       ░  ░             ░  ░    ░ ░   ░ ░\"" >> /etc/profile
+echo "echo \"                ░                                        ░ ░\"" >> /etc/profile
+echo "echo \"                                           redeploy.se\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
 echo "echo \"Your management URL for your Unifi Controller is https://\`curl -s ipecho.net/plain; echo\`:8443\"" >> /etc/profile
-echo "echo \"\"" >> /etc/profile
-echo "echo \"\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
