481523bdcee906131b8a4c249e118c3a6952bb9c
Jonas Erikson <jonas@redeploy.se>
2016-02-21 16:18:48 +0100

    commit

--- test/helloworld.ps1
+++ test/helloworld.ps1
@@ -8,6 +8,6 @@ param (
 
 mkdir c:\temp
 $GetArgument = $Argument
-#$SplitArgument = $Argument.Split(";")
-#Write-Output "Hello World" | out-file c:\temp\helloworld.txt
-Write-Output $GetArgument | out-file c:\temp\helloworld.txt
+$SplitArgument = $Argument.Split(";")
+Write-Output "Hello World" | out-file c:\temp\helloworld.txt
+Write-Output $SplitArgument | out-file c:\temp\helloworld.txt
