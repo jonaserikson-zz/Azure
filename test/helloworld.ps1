7bf9f1e655e97bba5ef0833a42b86e2784ddb16d
Jonas Erikson <jonas@redeploy.se>
2016-02-21 18:44:21 +0100

    commit

--- test/helloworld.ps1
+++ test/helloworld.ps1
@@ -10,3 +10,8 @@ $CredsPath = "c:\temp\"
 $SplitCredStuff = $CredStuff.Split(";")
 mkdir c:\temp
 Write-Output $SplitCredStuff[1] | out-file $CredsPath$SplitCredStuff[0]
+
+
+
+
+#Remove-Item -Recurse -Force c:\temp
