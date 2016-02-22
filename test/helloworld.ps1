26f2cf2823e9d11aaab68117ff9e3fac55f1e043
Jonas Erikson <jonas@redeploy.se>
2016-02-22 09:02:37 +0100

    commit

--- test/helloworld.ps1
+++ test/helloworld.ps1
@@ -9,9 +9,9 @@ param (
 
 $CredsPath = "c:\temp\"
 mkdir c:\temp
-#$SplitCredStuff = $CredStuff -split ';',2
+$SplitCredStuff = $CredStuff -split ';',2
 #Write-Output $SplitCredStuff[1] | out-file $CredsPath$($SplitCredStuff[0])
-Write-Output $credstuff | out-file c:\temp\test.txt
+Invoke-WebRequest -Uri $SplitCredStuff[0] | Out-File c:\temp\$SplitCredStuff[1]
 
 
 
