0843c158475aae04e8998c7bed717d3cab919cf4
Jonas Erikson <jonas.erikson@redeploy.se>
2016-12-02 13:14:18 +0100

    commit

--- PowerShell/list-all-locations-for-series.ps1
+++ PowerShell/list-all-locations-for-series.ps1
@@ -3,6 +3,9 @@ $Series = "Standard_N*"
 $locations = (Get-AzureRmLocation).location
 foreach ($location in $locations) {
 $Sizes = (Get-AzureRmVMSize -Location $location | Where {$_.Name -like $Series}).Name
-Write-output $location $Sizes
-write-output .......
+Write-output $location
+write-output ..............
+write-output $Sizes
+write-output ""
+write-output ""
  }
--- PowerShell/list-all-locations-for-size.ps1
+++ PowerShell/list-all-locations-for-size.ps1
@@ -8,6 +8,9 @@ $AllSizes = $AllSizes | select -unique
 $Size = $AllSizes | Sort-Object | Out-GridView -Title "Find all locations containing model?" -PassThru
 foreach ($location in $locations) {
 $Sizes = (Get-AzureRmVMSize -Location $location | Where {$_.Name -eq $Size}).Name
-Write-output $location $Sizes
-write-output .......
+Write-output $location
+write-output ..............
+write-output $Sizes
+write-output ""
+write-output ""
  }
